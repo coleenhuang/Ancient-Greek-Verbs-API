c754837b337dc4679aab75fe9306f6424170dc0f
Coleen Huang <coleenhuang@gmail.com>
2020-12-13 17:50:43 -0800

    added route to get all vocab words

--- routes/vocab.js
+++ routes/vocab.js
@@ -3,16 +3,25 @@ const { pool } = require('../config')
 const { response } = require('express');
 const router = express.Router();
 
+const getVocab = (request, response) => {
+  pool.query('SELECT * FROM vocab', (error, results) => {
+    if(error){
+      throw(error)
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
 const getSets = (request, response) => {
   pool.query('SELECT * FROM vocab_sets', (error, results) => {
     if (error){
       throw(error)
     }
-    response.status(200).json(results.row)
+    response.status(200).json(results.rows)
   })
 }
 
+router.get('/', getVocab);
 router.get('/sets', getSets);
 
 module.exports = router;
